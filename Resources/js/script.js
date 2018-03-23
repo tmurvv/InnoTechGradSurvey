9a988fecdd050a7beed8bc95065c48aba9596dd2
Tisha Murvihill <tmurv@take2tech.ca>
2018-03-23 16:24:35 -0700

    Styling Done

--- Resources/js/script.js
+++ Resources/js/script.js
@@ -7,8 +7,13 @@ function throwCap() {
     const capIcon = document.querySelector("#ion-university");
     const bounce = document.querySelector("#bounce");
     const spin = document.querySelector("#spin");
+    let newHeight = 42;
 
-    capIcon.classList.add("cap-icon");
+    capIcon.classList.remove("ion-university");
+    bounce.classList.remove("bounce");
+    spin.classList.remove("spin");
+
+    capIcon.classList.add("ion-university");
     bounce.classList.add("bounce");
     spin.classList.add("spin");
 
