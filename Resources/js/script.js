0e285f3ea46da8673db7b0ac9ebfaa5d1e89181b
Tisha Murvihill <tmurv@take2tech.ca>
2018-05-01 09:14:25 -0600

    Pop a cork! Version 1.0 is ready!

--- Resources/js/script.js
+++ Resources/js/script.js
@@ -2,31 +2,47 @@
 
 "use strict";
 
-function throwCap() {
-    
-    
-    const bounce = document.querySelector("#bounce");
-    const spin = document.querySelector("#spin");
-    const Cap = document.querySelector(".btnCap");
-    
-    
-    
-    bounce.classList.remove("bounce");
-    spin.classList.remove("spin");
-
-    bounce.classList.add("bounce");
-    spin.classList.add("spin");
-      
-}
-
 $(document).ready(function () {
-    
 
-    $(".js--nav-icon").click(function() {
+    $(".js--throwCap").click(function() {
+        var bounce = $("#bounce");
+        var spin = $("#spin");
+        var cap = $(".js--displayToggle");
+        var reset = $(".js--displayToggle--reset");
+
+        if (!$(bounce).hasClass("bounce")) {
+            $(bounce).addClass("bounce");
+        }
+        if (!$(spin).hasClass("spin")) {
+            $(spin).addClass("spin");
+        }
+    
+        $(cap).css("display", "none");
+        $(reset).css("display", "inline-block");
+    });
+
+    $(".js--throwCapReset").click(function () {
+        var bounce = $("#bounce");
+        var spin = $("#spin");
+        var cap = $(".js--displayToggle");
+        var reset = $(".js--displayToggle--reset"); 
+
+        if ($(bounce).hasClass("bounce")) {
+            $(bounce).removeClass("bounce");
+        }
+        if ($(spin).hasClass("spin")) {
+            $(spin).removeClass("spin");
+        }
+
+        $(cap).css("display", "inline-block");      
+        $(reset).css("display", "none");  
+    });
+
+    $(".js--nav-icon").click(function () {
         var nav = $(".js--main-nav");
         var icon = $(".js--nav-icon");
         nav.slideToggle(200);
-        
+
         if (icon.hasClass("ion-navicon")) {
             icon.addClass("ion-close");
             icon.removeClass("ion-navicon");
