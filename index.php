75fb97030896fd5d8584e0e5021a819998f2ad8d
tmurvv <tmurv@shaw.ca>
2018-05-13 00:12:44 -0600

    Just finished the don't loose your work on submission error improvement.

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+//
+
+?>
+
 <!DOCTYPE html>
 
 <html lang="en">
@@ -24,13 +29,6 @@
     <header>
         <nav>                                  
             <div class="row">
-
-
-
-
-
-
-            
                 <img src="Resources/Images/logopointerrgba.gif" alt="TechSchool Logo" class="logo">
                 <ul class="main-nav js--main-nav">
                     <li class="main-nav--item"><a href="#survey">Go to Grad Survey</a></li>
@@ -63,22 +61,85 @@
             <!-- <i class="ion-alert-circled"></i>THIS SITE IS A PROTOTYPE. USE TEST DATA. PLEASE DO NOT ENTER PERSONAL DATA.</p><br> -->
         </div>
         <div class="row">
-            <!-- <form method="post" action="emailer.php" class="contact-form"> -->
-            <form method="post" action="emailer.php" class="contact-form">
+        <?php
+            $name = $phone = $email = $addressline1 = $addressline2 = $addressline3 = $employed = $whereEmployed = $jobAssistance = $message = "";
+           
+            //Get the form fields, removes html tags and whitespace.
+            $name = strip_tags(trim($_POST["name"]));
+            $name = str_replace(array("\r","\n"),array(" "," "),$name);
+            $phone = strip_tags(trim($_POST["phone"]));
+            $phone = str_replace(array("\r","\n"),array(" "," "),$phone);
+            $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL); 
+
+            $addressline1 = strip_tags(trim($_POST["addressLine1"]));
+            $addressline1 = str_replace(array("\r","\n"),array(" "," "),$addressline1);
+            $addressline2 = strip_tags(trim($_POST["addressLine2"]));
+            $addressline2 = str_replace(array("\r","\n"),array(" "," "),$addressline2);
+            $addressline3 = strip_tags(trim($_POST["addressLine3"]));
+            $addressline3 = str_replace(array("\r","\n"),array(" "," "),$addressline3);
+
+            $employed = strip_tags(trim($_POST["employed"]));
+            $employed = str_replace(array("\r","\n"),array(" "," "),$employed);
+            $whereEmployed = strip_tags(trim($_POST["whereEmployed"]));
+            $whereEmployed = str_replace(array("\r","\n"),array(" "," "),$whereEmployed);
+            $jobAssistance = strip_tags(trim($_POST["jobAssistance"]));
+            $jobAssistance = str_replace(array("\r","\n"),array(" "," "),$jobAssistance);
+            
+            // Set the recipient email address. Update this to YOUR desired email address.
+            $recipient = "info@innotechcollege.com";
+            $notifytake2tech = "tech@take2tech.ca";   
+
+            // Set the email subject.
+            $subject = "Grad Survey Response from $name";
+
+            // Build the email content.  
+            $email_content = "Contact Info:\n\n";
+            $email_content .= "Name: $name\n";
+            $email_content .= "Phone: $phone\n";
+            $email_content .= "Email: $email\n\n";
+            $email_content .= "Address Line 1: $addressline1\n";
+            $email_content .= "Address Line 2: $addressline2\n";
+            $email_content .= "City/Province/Postal: $addressline3\n\n";
+            $email_content .= "Employment Info:\n\n";
+            $email_content .= "Are you employed? \n\n$employed\n\n";
+            $email_content .= "If yes, where? \n\n$whereEmployed\n\n";
+            $email_content .= "Do you need job placement assistance? \n\n$jobAssistance\n\n";
+
+            $notify_content = "This is a notification to take2tech.ca. A Grad placement survey has been sent to InnoTech College.";
+
+            // Build the email headers.
+            $email_headers = "From: $name <$email>";
+
+            if (isset($_POST['name'])) {
+                if (mail($recipient, $subject, $email_content, $email_headers)) {                   
+                    echo "<div class=\"messageToUser messageToUser--success\">Success! Your form has been submitted</div>";
+                                    
+                    $name = $phone = $email = $addressline1 = $addressline2 = $addressline3 = $employed = $whereEmployed = $jobAssistance = $message = "";
+                }
+                else {
+                    echo "<div class=\"messageToUser messageToUser--fail\"><i class=\"ion-alert-circled\">
+                    </i>Oops... something went wrong. Form not sent. Please double-check your e-mail and try again.</div>";
+                }
+                mail($notifytake2tech, $subject, $notify_content, $email_headers);
+            } 
+                        
+            ?>
+            
+            <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" class="contact-form">
 
                 <div class="row">
                    
                     <p class="question">1. Are you employed full-time or part-time?</p>
                     <div>
-                        <input type="radio" name="employed" value="Full-time">Full-time<br>
-                        <input type="radio" name="employed" value="Part-time">Part-time<br>
-                        <input type="radio" name="employed" value="Unemployed">Unemployed
+                        <input type="radio" name="employed" value="Full-time" <?php if (isset($employed) && $employed=="Full-time") echo "checked";?>>Full-time<br>
+                        <input type="radio" name="employed" value="Part-time" <?php if (isset($employed) && $employed=="Part-time") echo "checked";?>>Part-time<br>
+                        <input type="radio" name="employed" value="Unemployed" <?php if (isset($employed) && $employed=="Unemployed") echo "checked";?>>Unemployed
                     </div>
 
                     <p class="question">2. If you are employed, who and where is it?</p>
-                    <textarea name="whereEmployed"></textarea>
+                    <textarea name="whereEmployed"><?php echo $whereEmployed;?></textarea>
                     <p class="question">3. Do you require any assistance in Job Placement?</p>
-                    <textarea name="jobAssistance"></textarea>
+                    <textarea name="jobAssistance"><?php echo $jobAssistance;?></textarea>
                     <br>
                     <p class="question">4. Please fill out your most current contact info:</p>
                     <div class="row">
@@ -86,7 +147,7 @@
                             <label for="name">Name:<ion-asterisk></label>
                         </div>
                         <div class="col span-3-of-4">
-                            <input type="text" name="name" id="name" placeholder="Your name" required>
+                            <input type="text" name="name" id="name" value="<?php echo $name;?>" placeholder="Your name" required>
                         </div>
                     </div>
                     <div class="row">
@@ -94,7 +155,7 @@
                             <label for="phone">Phone #:</label>
                         </div>
                         <div class="col span-3-of-4">
-                            <input type="text" name="phone" id="phone" placeholder="Your phone number">
+                            <input type="text" name="phone" id="phone" value="<?php echo $phone;?>" placeholder="Your phone number">
                         </div>
                     </div>
                     <div class="row">
@@ -102,7 +163,7 @@
                             <label for="email">Email:<ion-asterisk></label>
                         </div>
                         <div class="col span-3-of-4">
-                            <input type="email" name="email" id="email" placeholder="Your email" required>
+                            <input type="email" name="email" id="email" value="<?php echo $email;?>" placeholder="Your email" required>
                         </div>
                     </div>
                         <div class="row">
@@ -115,7 +176,7 @@
                                 <label for="addressLine1">Line 1:</label>
                             </div>
                             <div class="col span-3-of-4">
-                                <input type="text" name="addressLine1" id="addressLine1" placeholder="Address line 1">
+                                <input type="text" name="addressLine1" id="addressLine1" value="<?php echo $addressline1;?>" placeholder="Address line 1">
                             </div>
                         </div>
                         <div class="row">
@@ -123,7 +184,7 @@
                                 <label for="addressLine2">Line 2:</label>
                             </div>
                             <div class="col span-3-of-4">
-                                <input type="text" name="addressLine2" id="addressLine2" placeholder="Address line 2">
+                                <input type="text" name="addressLine2" id="addressLine2" value="<?php echo $addressline2;?>" placeholder="Address line 2">
                             </div>
                         </div>
                         <div class="row">
@@ -131,7 +192,7 @@
                                 <label for="addressLine3">City Province Postal:</label>
                             </div>
                             <div class="col span-3-of-4">
-                                <input type="text" name="addressLine3" id="addressLine3" placeholder="City/Province/Postal">
+                                <input type="text" name="addressLine3" id="addressLine3" value="<?php echo $addressline3;?>" placeholder="City/Province/Postal">
                             </div>
                         </div>
                         <div class="row submitContainer">
@@ -143,14 +204,8 @@
                             </div>
                             <div class="col span-2-fo-4">
                                 <?php
-                                    if($_GET["success"] == 1) {
-                                        echo "<div class=\"messageToUser messageToUser--success\">Success! Your form has been submitted.</div>";
-                                    }
-
-                                    if($_GET["success"] == -1) {
-                                        echo "<div class=\"messageToUser messageToUser--fail\"><i class=\"ion-alert-circled\"></i>Oops... something went wrong. Form not sent.
-                                        Please double-check your e-mail and try again.</div>";
-                                    }                               
+                                    // Send the email.
+                                                              
                                 ?>
                             </div>
                         </div> 
